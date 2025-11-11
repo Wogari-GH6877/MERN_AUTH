e8efa924953ee360a8a454ee5cafe5fb976060e4
Wogari-GH6877 <wogariararsa0968@gmail.com>
2025-11-11 13:23:47 -0800

    Delete Routing directory

--- Routing/userRouting.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authMiddleWare from "../middleWare/authMiddleWare.js";
-import getUserData from "../Controller/userController.js";
-const userRouter=express.Router();
-
- userRouter.get("/data",authMiddleWare,getUserData);
-
-export default userRouter;
