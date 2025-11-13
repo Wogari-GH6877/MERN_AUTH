da061e4c82d3d05e12ac6ff01e104514c5a93451
Wogari-GH6877 <wogariararsa0968@gmail.com>
2025-11-13 05:10:18 -0800

    Depoly the Frontend

--- CLIENT/src/Context/appContext.jsx
+++ CLIENT/src/Context/appContext.jsx
@@ -16,6 +16,7 @@ export const AppContextProvider=(props)=>{
     const getAuthState = async () =>{
         try {
             const {data} = await axios.get(BackendUrl + "/api/auth/is-auth");
+            
             if(data.success){
                 setIsLoggedin(true);
                 getUserData();
@@ -38,10 +39,10 @@ export const AppContextProvider=(props)=>{
     const getUserData= async()=>{
 
         try {
-            const {data}=await axios.get(BackendUrl + "/api/user/data")
+            const {data}=await axios.get(BackendUrl + "/api/user/data");
             data.success?setUserData(data.userData) : toast.error(data.message)
         } catch (error) {
-            if(data.status!==401){
+            if(error.response?.status!==401){
             toast.error(error.response?.data?.message || error.message);}
         }
     }
